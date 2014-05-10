ddf478713e46e70b84efa09963ea6d1b6339283c
Seanna Musgrave <seannamusgrave@gmail.com>
2014-05-10 01:03:43 -0700

    Added moving around and lights

---
+++ unity/Assets/Scripts/Mouse.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse : MonoBehaviour {
+
+	Vector3 target = new Vector3(0, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if (Input.GetMouseButtonDown (0)) {
+			    Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				target = ray.origin;
+
+			}
+	}
+
+	public Vector3 getTarget() {
+		return target;
+	}
+}
---
+++ unity/Assets/Scripts/YetiLight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YetiLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D collision) {
+		Debug.Log (collision.gameObject.GetComponent<SpriteRenderer> ().color);
+		gameObject.GetComponent<SpriteRenderer> ().color = collision.gameObject.GetComponent<SpriteRenderer> ().color;
+	}
+
+	void OnTriggerExit2D() {
+		gameObject.GetComponent<SpriteRenderer> ().color = new Color (1, 1, 1);
+
+	}
+}
